9ab98bf766600a069568091aaf9aa29c2d95e386
remi.odufuye <remi.odufuye@gmail.com>
2019-10-01 15:10:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var name = "Susan"  ;
 var height = "74" ;
 
-if ( name === "Susan") {
-  console.log("The name is correct") ; 
-} else {
-  console.log("The Expected Name is Susan") ; 
-}
-
 
 // Don't worry about this
 module.exports = { name, height
